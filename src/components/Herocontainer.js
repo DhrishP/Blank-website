2dab83818a1550b82d712b9763d9bf1d1e45349d
Dhrish Parekh <dhrishparekh.ss@gmail.com>
2023-03-06 22:24:53 +0530

    final

--- src/components/Herocontainer.js
+++ src/components/Herocontainer.js
@@ -13,9 +13,9 @@ function Herocontainer() {
     <div className=' flex flex-col items-center   md:grid grid-cols-2 gap-y-5 '>
         <div className=' w-1/2 mt-20 md:ml-20 text-white'>
        <h1 className='text-6xl font-bold '>Protect Yourself using Bla<span className='text-blue-400'>nk.</span></h1>
-       <h2 className='mt-6 text-center md:font-semibold text-md '>India's best Security Website. Lorem ipsum dolor sit amet consectetur adipisicing elit. A nobis exercitationem laborum ratione harum! Ipsum veniam voluptatem eligendi eius ut! <span className='hidden lg:block'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam tempora deserunt, quos, cumque repellat et nesciunt dolorem ut architecto repellendus, quam id eius. </span></h2>
+       <h2 className='mt-6 text-left md:font-semibold text-md '>India's best cloud services website sit amet. Lorem ipsum dolor sit amet consectetur adipisicing elit.A nobis exercitationem laborum ratione harum! Ipsum veniam voluptatem eligendi eius ut! <span className='hidden lg:block'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam tempora deserunt, quos, cumque repellat et nesciunt dolorem ut architecto repellendus, quam id eius. </span></h2>
        <div className='buuton '>
-        <button className="p-2 w-64 my-5 font-bold transition hover:bg-slate-900 hover:border  rounded-xl bg-blue-400 block m-auto ">Get started</button>
+        <button className="p-2 w-56 md:w-64 my-5 font-bold transition hover:bg-slate-900 hover:border  md:rounded-xl bg-blue-400   ">Get started</button>
 </div>
 </div>
 <div className='mt-6 ml-7 md:ml-10 '>
